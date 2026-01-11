0f4871356ec77aff8c37441def14b075c94756ab
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2026-01-11 23:07:47 +0000

    Final data cleanup before separation

--- analysis/transfer/2-cleanup.lua
+++ analysis/transfer/2-cleanup.lua
@@ -17,8 +17,11 @@ local f = fs.readFileSync("1-normalised.json")
 local t = json.decode(f)
 
 local constructed = {}
-local global_pricing = {}
+-- local global_pricing = {}
 
+local null = json.null
+
+local tinsert = table.insert
 for _, e in pairs(t) do
     local new = {}
 
@@ -26,10 +29,7 @@ for _, e in pairs(t) do
     new.id = string.lower(name)
     new.name = name
 
-    new.urls = {
-        website = e.links.href,
-        purchase = e.links.priceHref
-    }
+    new.website = e.links.href
 
     local sunc = {}
     local platforms = e.main.plat
@@ -44,11 +44,54 @@ for _, e in pairs(t) do
         }
     end
     new.sunc = sunc
+    new.platforms = platforms
 
     if uncLinkCount ~= #platforms then
         warn("%-10s | %5d platforms | %5d sUNC links | %4d", name, #platforms, uncLinkCount,
             math.abs(#platforms - uncLinkCount))
     end
+
+
+    local tags = {}
+    if e.flags.hasKeySystem then
+        tinsert(tags, "key-system")
+    end
+    new.tags = tags
+
+    local badges = {}
+    if e.flags.verified then
+        tinsert(badges, "verified")
+    end
+    if e.flags.premium then
+        tinsert(badges, "premium")
+    end
+    new.badges = badges
+
+    new.type = e.flags.external and "external" or "internal"
+
+    new.visibility = {
+        hidden = e.flags.hide
+    }
+
+    -- THESE ARE ENTIRELYT SEPARATE FILES
+    -- theyre only aggregated into the cleaned up file for later processing
+    new.description_md = e.modals.info or null
+    local modals = {}
+    if e.modals.warning then
+        local w = e.modals.warning
+        w.icon = nil
+        w.continue = nil
+        modals.warning = w
+    else
+        modals.warning = null
+    end
+    new.modals = modals
+
+    new.misc_data = {
+        pros = e.main.pros,
+        cons = e.main.cons,
+        neutrals = e.main.neutral
+    }
 
     table.insert(constructed, new)
 end
@@ -56,4 +99,4 @@ end
 -- p(constructed)
 
 fs.writeFileSync("2-cleaned.json", json.encode(constructed, { indent = true }))
-fs.writeFileSync("2-globalpricing.json", json.encode(global_pricing, { indent = true }))
+-- fs.writeFileSync("2-globalpricing.json", json.encode(global_pricing, { indent = true }))
