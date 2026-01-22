ebf5cd33054bf8dffc40468969373c0b49809cf8
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2026-01-22 19:30:20 +0000

    .o.mjs files

---
+++ src/assets/js/constants.o.mjs
@@ -0,0 +1,21 @@
+export const friendlyTags = {
+    decompiler: ["Decompiler", "fa-code"],
+    undetected: ["Undetected", "fa-user-secret"],
+    kernel: ["Kernel", "fa-microchip"],
+    ai: ["AI", "fa-robot"],
+    "multi-instance": ["Multi-instance", "fa-layer-group"],
+    "supports-vng": ["Supports VNG", "fa-globe-asia"] // Roblox Vietnam (separate client)
+}
+
+export const currencySymbols = {
+    USD: "$",
+    EUR: "€",
+    GBP: "£"
+}
+
+export const platformLabels = {
+    windows: "Windows",
+    macos: "macOS",
+    android: "Android",
+    ios: "iOS",
+};
