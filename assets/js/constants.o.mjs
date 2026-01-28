6449b7e3bb78b2dd1e12daad1426950657bc14ec
Richy-Z <Richy-Z@users.noreply.github.com>
2026-01-28 22:43:09 +0000

    deploy: ed5e1a9df570b57b50294b34fd9c161727fff21d

---
+++ assets/js/constants.o.mjs
@@ -0,0 +1,20 @@
+export const friendlyTags = {
+    decompiler: ["Decompiler", "fa-code"],
+    kernel: ["Kernel", "fa-microchip"],
+    ai: ["AI", "fa-robot"],
+    "multi-instance": ["Multi-instance", "fa-layer-group"],
+    "supports-vng": ["Supports VNG", "fa-globe-asia"] // Roblox Vietnam (separate client)
+}
+
+export const currencySymbols = {
+    USD: "$",
+    EUR: "€",
+    GBP: "£"
+}
+
+export const platformLabels = {
+    windows: "Windows",
+    macos: "macOS",
+    android: "Android",
+    ios: "iOS",
+};
